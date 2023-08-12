5e74edfabaf20fc797ccc4dfcb17cba118029c95
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-08-12 16:51:42 -0700

    Actividad 2 (Canvas)

---
+++ act_2/main.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("canvas");
+
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "rgb(111,123,221)";
+ctx.fillRect(10, 10, 50, 50);
+
+ctx.fillStyle = "rgb(210,10,25)";
+ctx.fillRect(50, 50, 50, 50);
+
+ctx.fillStyle = "rgba(255,138,253,0.85)";
+ctx.fillRect(90, 90, 50, 50);
